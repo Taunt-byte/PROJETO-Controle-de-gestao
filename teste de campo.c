9c46a992d03fa91af618e8923feac6f31bdaf008
Taunt-byte <71947586+Taunt-byte@users.noreply.github.com>
2020-09-26 18:29:43 -0300

    Add files via upload

---
+++ teste de campo.c
@@ -0,0 +1,8 @@
+#include <stdio.h> /* Pacotes com funções de entrada e saída */
+
+int main(void)
+{
+    puts("Olá, Mundo!");
+    return 0; /* Retorna 0, pois `main` retorna um `int` */
+}
+/* Nova linha após fechar a chave principal */
